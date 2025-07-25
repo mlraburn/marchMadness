2edaa14c3fe99e8fadd458f6d68c4372a8e0c24b
Matthew Raburn <matthew.raburn@gmail.com>
2025-07-24 23:58:46 -0400

    started created user storage capability

---
+++ backend/bracket_storage.py
@@ -0,0 +1,13 @@
+"""
+File contains functions for storing and loading bracket data
+
+Bracket schema
+
+Each team gets a positional name
+E01 indicates the East regions number one seed
+
+The extra 68 teams get a positional name that has an extra A or B at the end
+so E16A for a team that is in the 0th round of the tournament
+
+"""
+
